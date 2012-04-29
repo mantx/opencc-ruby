478e3389e74d082b861330463c08d7b74ba5e41a
Shawn Wang <shonglew@gmail.com>
2012-04-29 08:05:00 +0100

    add a ruby convert class for opencc

--- lib/opencc-ruby.rb
+++ lib/opencc-ruby.rb
@@ -1,4 +1,15 @@
 require 'opencc-ruby/opencc'
 
 module Opencc
+  class Convert
+    def initialize(config=nil) 
+      @od = Opencc.open config
+    end  
+    def convert(str) 
+      return Opencc.convert @od, str
+    end
+    def destroy 
+      Opencc.close $od
+    end
+  end
 end
